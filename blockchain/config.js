0bdbd8f996e0183211c949cd26b3fc97d4e2e7f7
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2024-12-08 10:57:02 -0800

    Update config.js

--- blockchain/config.js
+++ blockchain/config.js
@@ -3,6 +3,6 @@ module.exports = {
   chainId: "prysm-devnet-1",
   bech32Prefix: "prysm",
   relayers: ["https://my-bitcoin-relayer.example.com:1234"], //Nomic relayer
-  ibcChannel: "channel-0", // Update with the correct IBC channel for your chain
+  ibcChannel: "channel-0", // Update with the correct IBC channel for your chain. channel-0 is prysm<>osmosis relay channel
   bitcoinNetwork: "testnet",
 };
